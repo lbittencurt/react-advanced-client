5d21af7864545d1d1b3575ff8e7266483c60ed98
Lucas Bittencurt <bittencurt.lucas@outlook.com>
2020-08-06 12:49:33 -0300

    husky class

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+type Props = {
+  title: string
+}
+
+export default function Home({ title = 'Advanced React' }: Props) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>React Avançado!</h1>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
